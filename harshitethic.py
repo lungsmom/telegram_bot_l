26b940ee4aa65032aac2408b8c46e0d0f891b212
lungsmom <128880980+lungsmom@users.noreply.github.com>
2023-03-25 13:43:37 +0900

    Update harshitethic.py

--- harshitethic.py
+++ harshitethic.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "ENTER YOUR API KEY HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "ENTER YOUR TELEGRAM TOKEN HERE"
+    tgkey = "5997686392:AAF_EO2fmr8nSFgcSu6-rqRDqPVqjN5i8a0"
 else:
     tgkey = tgenv
 print(tgenv)
